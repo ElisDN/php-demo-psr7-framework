5fb6c6926423c08ee00bc6821d8886e42787a8f3
ElisDN <mail@elisdn.ru>
2017-10-08 21:39:12 +0300

    Added blog actions

--- public/index.php
+++ public/index.php
@@ -16,10 +16,30 @@ $request = ServerRequestFactory::fromGlobals();
 $path = $request->getUri()->getPath();
 
 if ($path === '/') {
+
     $name = $request->getQueryParams()['name'] ?? 'Guest';
     $response = new HtmlResponse('Hello, ' . $name . '!');
+
 } elseif ($path === '/about') {
+
     $response = new HtmlResponse('I am a simple site');
+
+} elseif ($path === '/blog') {
+
+    $response = new JsonResponse([
+        ['id' => 2, 'title' => 'The Second Post'],
+        ['id' => 1, 'title' => 'The First Post'],
+    ]);
+
+} elseif (preg_match('#^/blog/(?P<id>\d+)$#i', $path, $matches)) {
+
+    $id = $matches['id'];
+    if ($id > 2) {
+        $response = new JsonResponse(['error' => 'Undefined page'], 404);
+    } else {
+        $response = new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
+    }
+
 } else {
     $response = new HtmlResponse('Undefined page', 404);
 }
