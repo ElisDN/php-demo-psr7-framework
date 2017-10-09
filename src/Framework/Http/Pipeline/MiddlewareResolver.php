3b65727df7150099927fa0e2ba369c724d4ce4b6
ElisDN <mail@elisdn.ru>
2017-10-09 17:54:48 +0300

    Added lazy resolving

--- src/Framework/Http/Pipeline/MiddlewareResolver.php
+++ src/Framework/Http/Pipeline/MiddlewareResolver.php
@@ -2,10 +2,19 @@
 
 namespace Framework\Http\Pipeline;
 
+use Psr\Http\Message\ServerRequestInterface;
+
 class MiddlewareResolver
 {
     public function resolve($handler): callable
     {
-        return \is_string($handler) ? new $handler() : $handler;
+        if (\is_string($handler)) {
+            return function (ServerRequestInterface $request, callable $next) use ($handler) {
+                $object = new $handler();
+                return $object($request, $next);
+            };
+        }
+
+        return $handler;
     }
 }
