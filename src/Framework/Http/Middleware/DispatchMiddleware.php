c4856e35b98daa805cde73a7f6983f1767d7a0f0
ElisDN <mail@elisdn.ru>
2017-10-12 23:23:13 +0300

    Separated dispatch middleware

---
+++ src/Framework/Http/Middleware/DispatchMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Framework\Http\Middleware;
+
+use Framework\Http\Pipeline\MiddlewareResolver;
+use Framework\Http\Router\Result;
+use Psr\Http\Message\ServerRequestInterface;
+
+class DispatchMiddleware
+{
+    private $resolver;
+
+    public function __construct(MiddlewareResolver $resolver)
+    {
+        $this->resolver = $resolver;
+    }
+
+    public function __invoke(ServerRequestInterface $request, callable $next)
+    {
+        /** @var Result $result */
+        if (!$result = $request->getAttribute(Result::class)) {
+            return $next($request);
+        }
+        $middleware = $this->resolver->resolve($result->getHandler());
+        return $middleware($request, $next);
+    }
+}
--- src/Framework/Http/Middleware/RouteMiddleware.php
+++ src/Framework/Http/Middleware/RouteMiddleware.php
@@ -2,20 +2,18 @@
 
 namespace Framework\Http\Middleware;
 
-use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\Exception\RequestNotMatchedException;
+use Framework\Http\Router\Result;
 use Framework\Http\Router\Router;
 use Psr\Http\Message\ServerRequestInterface;
 
 class RouteMiddleware
 {
     private $router;
-    private $resolver;
 
-    public function __construct(Router $router, MiddlewareResolver $resolver)
+    public function __construct(Router $router)
     {
         $this->router = $router;
-        $this->resolver = $resolver;
     }
 
     public function __invoke(ServerRequestInterface $request, callable $next)
@@ -25,8 +23,7 @@ class RouteMiddleware
             foreach ($result->getAttributes() as $attribute => $value) {
                 $request = $request->withAttribute($attribute, $value);
             }
-            $middleware = $this->resolver->resolve($result->getHandler());
-            return $middleware($request, $next);
+            return $next($request->withAttribute(Result::class, $result));
         } catch (RequestNotMatchedException $e){
             return $next($request);
         }
