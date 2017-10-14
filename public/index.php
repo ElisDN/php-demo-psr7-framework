cc0829a32160cd4b87adfd2c37fec922b518a5cd
ElisDN <mail@elisdn.ru>
2017-10-14 22:36:51 +0300

    Added path pipe

--- public/index.php
+++ public/index.php
@@ -24,10 +24,7 @@ $routes = $aura->getMap();
 
 $routes->get('home', '/', Action\HelloAction::class);
 $routes->get('about', '/about', Action\AboutAction::class);
-$routes->get('cabinet', '/cabinet', [
-    new Middleware\BasicAuthMiddleware($params['users']),
-    Action\CabinetAction::class,
-]);
+$routes->get('cabinet', '/cabinet', Action\CabinetAction::class);
 $routes->get('blog', '/blog', Action\Blog\IndexAction::class);
 $routes->get('blog_show', '/blog/{id}', Action\Blog\ShowAction::class)->tokens(['id' => '\d+']);
 
@@ -40,6 +37,7 @@ $app->pipe(new Middleware\ErrorHandlerMiddleware($params['debug']));
 $app->pipe(Middleware\CredentialsMiddleware::class);
 $app->pipe(Middleware\ProfilerMiddleware::class);
 $app->pipe(new Framework\Http\Middleware\RouteMiddleware($router));
+$app->pipe('cabinet', new Middleware\BasicAuthMiddleware($params['users']));
 $app->pipe(new Framework\Http\Middleware\DispatchMiddleware($resolver));
 
 ### Running
