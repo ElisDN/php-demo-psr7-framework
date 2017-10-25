5a30b3e8b59d61494d6676a39ecd9b56167ccdd9
ElisDN <mail@elisdn.ru>
2017-10-25 19:23:00 +0300

    Extracted development config

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -5,7 +5,6 @@ use Framework\Http\Middleware\ErrorHandler\ErrorResponseGenerator;
 use Infrastructure\Framework\Http\Middleware\ErrorHandler\PrettyErrorResponseGenerator;
 use Framework\Http\Application;
 use Framework\Http\Middleware\ErrorHandler\ErrorHandlerMiddleware;
-use Framework\Http\Middleware\ErrorHandler\WhoopsErrorResponseGenerator;
 use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Router\Router;
@@ -40,12 +39,6 @@ return [
                 return $middleware;
             },
             ErrorResponseGenerator::class => function (ContainerInterface $container) {
-                if ($container->get('config')['debug']) {
-                    return new WhoopsErrorResponseGenerator(
-                        $container->get(Whoops\RunInterface::class),
-                        new Zend\Diactoros\Response()
-                    );
-                }
                 return new PrettyErrorResponseGenerator(
                     $container->get(TemplateRenderer::class),
                     new Zend\Diactoros\Response(),
@@ -55,14 +48,6 @@ return [
                         'error' => 'error/error',
                     ]
                 );
-            },
-            Whoops\RunInterface::class => function () {
-                $whoops = new Whoops\Run();
-                $whoops->writeToOutput(false);
-                $whoops->allowQuit(false);
-                $whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
-                $whoops->register();
-                return $whoops;
             },
             Psr\Log\LoggerInterface::class => function (ContainerInterface $container)
             {
