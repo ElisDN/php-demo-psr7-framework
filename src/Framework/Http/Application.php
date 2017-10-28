0fa5762272a4fcb7c5634fed0c073a08061fd1c8
ElisDN <mail@elisdn.ru>
2017-10-28 15:49:00 +0300

    Included default handler

--- src/Framework/Http/Application.php
+++ src/Framework/Http/Application.php
@@ -4,19 +4,28 @@ namespace Framework\Http;
 
 use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Pipeline\Pipeline;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 class Application extends Pipeline
 {
     private $resolver;
+    private $default;
 
-    public function __construct(MiddlewareResolver $resolver)
+    public function __construct(MiddlewareResolver $resolver, callable $default)
     {
         parent::__construct();
         $this->resolver = $resolver;
+        $this->default = $default;
     }
 
     public function pipe($middleware): void
     {
         parent::pipe($this->resolver->resolve($middleware));
+    }
+
+    public function run(ServerRequestInterface $request): ResponseInterface
+    {
+        return $this($request, $this->default);
     }
 }
