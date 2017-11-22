78b41a10babd6202129287b19c5076c489937ee2
ElisDN <mail@elisdn.ru>
2017-11-22 14:31:42 +0300

    Incapsulated route behavior

--- src/Framework/Http/Router/Route.php
+++ src/Framework/Http/Router/Route.php
@@ -2,13 +2,15 @@
 
 namespace Framework\Http\Router;
 
+use Psr\Http\Message\ServerRequestInterface;
+
 class Route
 {
-    public $name;
-    public $pattern;
-    public $handler;
-    public $tokens;
-    public $methods;
+    private $name;
+    private $pattern;
+    private $handler;
+    private $tokens;
+    private $methods;
 
     public function __construct($name, $pattern, $handler, array $methods, array $tokens = [])
     {
@@ -17,5 +19,49 @@ class Route
         $this->handler = $handler;
         $this->tokens = $tokens;
         $this->methods = $methods;
+    }
+    
+    public function match(ServerRequestInterface $request): ?Result
+    {
+        if ($this->methods && !\in_array($request->getMethod(), $this->methods, true)) {
+            return null;
+        }
+        
+        $pattern = preg_replace_callback('~\{([^\}]+)\}~', function ($matches) {
+            $argument = $matches[1];
+            $replace = $this->tokens[$argument] ?? '[^}]+';
+            return '(?P<' . $argument . '>' . $replace . ')';
+        }, $this->pattern);
+
+        $path = $request->getUri()->getPath();
+        
+        if (!preg_match('~^' . $pattern . '$~i', $path, $matches)) {
+            return null;
+        }
+
+        return new Result(
+            $this->name,
+            $this->handler,
+            array_filter($matches, '\is_string', ARRAY_FILTER_USE_KEY)
+        );
+    }
+
+    public function generate($name, array $params = []): ?string
+    {
+        $arguments = array_filter($params);
+
+        if ($name !== $this->name) {
+            return null;
+        }
+
+        $url = preg_replace_callback('~\{([^\}]+)\}~', function ($matches) use (&$arguments) {
+            $argument = $matches[1];
+            if (!array_key_exists($argument, $arguments)) {
+                throw new \InvalidArgumentException('Missing parameter "' . $argument . '"');
+            }
+            return $arguments[$argument];
+        }, $this->pattern);
+
+        return $url;
     }
 }
--- src/Framework/Http/Router/Router.php
+++ src/Framework/Http/Router/Router.php
@@ -18,23 +18,8 @@ class Router
     public function match(ServerRequestInterface $request): Result
     {
         foreach ($this->routes->getRoutes() as $route) {
-            if ($route->methods && !\in_array($request->getMethod(), $route->methods, true)) {
-                continue;
-            }
-
-            $pattern = preg_replace_callback('~\{([^\}]+)\}~', function ($matches) use ($route) {
-                $argument = $matches[1];
-                $replace = $route->tokens[$argument] ?? '[^}]+';
-                return '(?P<' . $argument . '>' . $replace . ')';
-            }, $route->pattern);
-
-            $path = $request->getUri()->getPath();
-            if (preg_match('~^' . $pattern . '$~i', $path, $matches)) {
-                return new Result(
-                    $route->name,
-                    $route->handler,
-                    array_filter($matches, '\is_string', ARRAY_FILTER_USE_KEY)
-                );
+            if ($result = $route->match($request)) {
+                return $result;
             }
         }
 
@@ -43,22 +28,8 @@ class Router
 
     public function generate($name, array $params = []): string
     {
-        $arguments = array_filter($params);
-
         foreach ($this->routes->getRoutes() as $route) {
-            if ($name !== $route->name) {
-                continue;
-            }
-
-            $url = preg_replace_callback('~\{([^\}]+)\}~', function ($matches) use (&$arguments) {
-                $argument = $matches[1];
-                if (!array_key_exists($argument, $arguments)) {
-                    throw new \InvalidArgumentException('Missing parameter "' . $argument . '"');
-                }
-                return $arguments[$argument];
-            }, $route->pattern);
-
-            if ($url !== null) {
+            if (null !== $url = $route->generate($name, array_filter($params))) {
                 return $url;
             }
         }
