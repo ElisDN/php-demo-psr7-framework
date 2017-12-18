4cd1c4b3ad470d6c67b317e71b5c12143f0e8ea1
ElisDN <mail@elisdn.ru>
2017-12-18 18:29:24 +0300

    Added auth decorator

--- public/index.php
+++ public/index.php
@@ -23,7 +23,10 @@ $routes = $aura->getMap();
 $routes->get('home', '/', Action\HelloAction::class);
 $routes->get('about', '/about', Action\AboutAction::class);
 
-$routes->get('cabinet', '/cabinet', new Action\CabinetAction($params['users']));
+$routes->get('cabinet', '/cabinet', new Action\BasicAuthActionDecorator(
+    new Action\CabinetAction(),
+    $params['users']
+));
 
 $routes->get('blog', '/blog', Action\Blog\IndexAction::class);
 $routes->get('blog_show', '/blog/{id}', Action\Blog\ShowAction::class)->tokens(['id' => '\d+']);
--- src/App/Http/Action/CabinetAction.php
+++ src/App/Http/Action/CabinetAction.php
@@ -3,31 +3,14 @@
 namespace App\Http\Action;
 
 use Psr\Http\Message\ServerRequestInterface;
-use Zend\Diactoros\Response\EmptyResponse;
 use Zend\Diactoros\Response\HtmlResponse;
 
 class CabinetAction
 {
-    private $users;
-
-    public function __construct(array $users)
-    {
-        $this->users = $users;
-    }
-
     public function __invoke(ServerRequestInterface $request)
     {
-        $username = $request->getServerParams()['PHP_AUTH_USER'] ?? null;
-        $password = $request->getServerParams()['PHP_AUTH_PW'] ?? null;
+        $username = $request->getAttribute('username');
 
-        if (!empty($username) && !empty($password)) {
-            foreach ($this->users as $name => $pass) {
-                if ($username === $name && $password === $pass) {
-                    return new HtmlResponse('I am logged in as ' . $username);
-                }
-            }
-        }
-
-        return new EmptyResponse(401, ['WWW-Authenticate' => 'Basic realm=Restricted area']);
+        return new HtmlResponse('I am logged in as ' . $username);
     }
 }
