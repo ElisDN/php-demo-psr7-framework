49624d4fe1e19c341b33e4300b232ba0434cbfeb
ElisDN <mail@elisdn.ru>
2017-12-19 20:08:16 +0300

    Added single instances support

--- src/Framework/Container/Container.php
+++ src/Framework/Container/Container.php
@@ -5,9 +5,14 @@ namespace Framework\Container;
 class Container
 {
     private $definitions = [];
+    private $results = [];
 
     public function get($id)
     {
+        if (array_key_exists($id, $this->results)) {
+            return $this->results[$id];
+        }
+
         if (!array_key_exists($id, $this->definitions)) {
             throw new ServiceNotFoundException('Unknown service "' . $id . '"');
         }
@@ -15,16 +20,19 @@ class Container
         $definition = $this->definitions[$id];
 
         if ($definition instanceof \Closure) {
-            $result = $definition();
+            $this->results[$id] = $definition();
         } else {
-            $result = $definition;
+            $this->results[$id] = $definition;
         }
 
-        return $result;
+        return $this->results[$id];
     }
 
     public function set($id, $value): void
     {
+        if (array_key_exists($id, $this->results)) {
+            unset($this->results[$id]);
+        }
         $this->definitions[$id] = $value;
     }
 }
