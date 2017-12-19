b4ed2ee695b6448119d6ccb3a88c8d78758dba38
ElisDN <mail@elisdn.ru>
2017-12-19 21:54:05 +0300

    Added autowiring

--- config/dependencies.php
+++ config/dependencies.php
@@ -3,8 +3,6 @@
 use App\Http\Middleware;
 use Framework\Container\Container;
 use Framework\Http\Application;
-use Framework\Http\Middleware\DispatchMiddleware;
-use Framework\Http\Middleware\RouteMiddleware;
 use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Router\Router;
@@ -34,12 +32,4 @@ $container->set(Middleware\BasicAuthMiddleware::class, function (Container $cont
 
 $container->set(Middleware\ErrorHandlerMiddleware::class, function (Container $container) {
     return new Middleware\ErrorHandlerMiddleware($container->get('config')['debug']);
-});
-
-$container->set(DispatchMiddleware::class, function (Container $container) {
-    return new DispatchMiddleware($container->get(MiddlewareResolver::class));
-});
-
-$container->set(RouteMiddleware::class, function (Container $container) {
-    return new RouteMiddleware($container->get(Router::class));
 });
--- src/Framework/Container/Container.php
+++ src/Framework/Container/Container.php
@@ -15,7 +15,24 @@ class Container
 
         if (!array_key_exists($id, $this->definitions)) {
             if (class_exists($id)) {
-                return $this->results[$id] = new $id();
+                $reflection = new \ReflectionClass($id);
+                $arguments = [];
+                if (($constructor = $reflection->getConstructor()) !== null) {
+                    foreach ($constructor->getParameters() as $parameter) {
+                        if ($paramClass = $parameter->getClass()) {
+                            $arguments[] = $this->get($paramClass->getName());
+                        } elseif ($parameter->isArray()) {
+                            $arguments[] = [];
+                        } else {
+                            if (!$parameter->isDefaultValueAvailable()) {
+                                throw new ServiceNotFoundException('Unable to resolve "' . $parameter->getName() . '"" in service "' . $id . '"');
+                            }
+                            $arguments[] = $parameter->getDefaultValue();
+                        }
+                    }
+                }
+                $this->results[$id] = $reflection->newInstanceArgs($arguments);
+                return $this->results[$id];
             }
             throw new ServiceNotFoundException('Unknown service "' . $id . '"');
         }
