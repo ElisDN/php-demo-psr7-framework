1666c557ce0f8834d5be4d9017fc77f15ad553e5
ElisDN <mail@elisdn.ru>
2017-12-19 21:54:05 +0300

    Added autowiring

--- tests/Framework/Container/ContainerTest.php
+++ tests/Framework/Container/ContainerTest.php
@@ -78,6 +78,46 @@ class ContainerTest extends TestCase
         self::assertSame($value1, $value2);
     }
 
+    public function testAutowiring()
+    {
+        $container = new Container();
+
+        $outer = $container->get(Outer::class);
+
+        self::assertNotNull($outer);
+        self::assertInstanceOf(Outer::class, $outer);
+
+        self::assertNotNull($middle = $outer->middle);
+        self::assertInstanceOf(Middle::class, $middle);
+
+        self::assertNotNull($inner = $middle->inner);
+        self::assertInstanceOf(Inner::class, $inner);
+    }
+
+    public function testAutowiringScalarWithDefault()
+    {
+        $container = new Container();
+
+        $scalar = $container->get(ScalarWithArrayAndDefault::class);
+
+        self::assertNotNull($scalar);
+
+        self::assertNotNull($inner = $scalar->inner);
+        self::assertInstanceOf(Inner::class, $inner);
+
+        self::assertEquals([], $scalar->array);
+        self::assertEquals(10, $scalar->default);
+    }
+
+    public function testAutowiringScalarWithoutDefault()
+    {
+        $container = new Container();
+
+        $this->expectException(ServiceNotFoundException::class);
+
+        $container->get(ScalarWithOutDefault::class);
+    }
+
     public function testNotFound()
     {
         $container = new Container();
@@ -85,6 +125,57 @@ class ContainerTest extends TestCase
         $this->expectException(ServiceNotFoundException::class);
 
         $container->get('email');
+    }
+}
+
+class Outer
+{
+    public $middle;
+
+    public function __construct(Middle $middle)
+    {
+        $this->middle = $middle;
+    }
+}
+
+class Middle
+{
+    public $inner;
+
+    public function __construct(Inner $inner)
+    {
+        $this->inner = $inner;
+    }
+}
+
+class Inner
+{
+
+}
+
+class ScalarWithArrayAndDefault
+{
+    public $inner;
+    public $array;
+    public $default;
+
+    public function __construct(Inner $inner, array $array, $default = 10)
+    {
+        $this->inner = $inner;
+        $this->array = $array;
+        $this->default = $default;
+    }
+}
+
+class ScalarWithOutDefault
+{
+    public $inner;
+    public $some;
+
+    public function __construct(Inner $inner, $some)
+    {
+        $this->inner = $inner;
+        $this->some = $some;
     }
 }
 
