c571d73f6b6d8d23a0e1a6aa1bb9b8f8424eeea8
ElisDN <mail@elisdn.ru>
2018-01-12 19:55:43 +0300

    Added container constructor definitions

--- config/container.php
+++ config/container.php
@@ -2,10 +2,8 @@
 
 use Framework\Container\Container;
 
-$container = new Container();
+$container = new Container(require __DIR__ . '/dependencies.php');
 
 $container->set('config', require __DIR__ . '/parameters.php');
-
-require __DIR__ . '/dependencies.php';
 
 return $container;
--- config/dependencies.php
+++ config/dependencies.php
@@ -7,29 +7,29 @@ use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Router\Router;
 
-/** @var Container $container */
+return [
+    Application::class => function (Container $container) {
+        return new Application(
+            $container->get(MiddlewareResolver::class),
+            $container->get(Framework\Http\Router\Router::class),
+            new Middleware\NotFoundHandler(),
+            new Zend\Diactoros\Response()
+        );
+    },
 
-$container->set(Application::class, function (Container $container) {
-    return new Application(
-        $container->get(MiddlewareResolver::class),
-        $container->get(Framework\Http\Router\Router::class),
-        new Middleware\NotFoundHandler(),
-        new Zend\Diactoros\Response()
-    );
-});
+    Router::class => function () {
+        return new AuraRouterAdapter(new Aura\Router\RouterContainer());
+    },
 
-$container->set(Router::class, function () {
-    return new AuraRouterAdapter(new Aura\Router\RouterContainer());
-});
+    MiddlewareResolver::class => function (Container $container) {
+        return new MiddlewareResolver($container);
+    },
 
-$container->set(MiddlewareResolver::class, function (Container $container) {
-    return new MiddlewareResolver($container);
-});
+    Middleware\BasicAuthMiddleware::class => function (Container $container) {
+        return new Middleware\BasicAuthMiddleware($container->get('config')['users']);
+    },
 
-$container->set(Middleware\BasicAuthMiddleware::class, function (Container $container) {
-    return new Middleware\BasicAuthMiddleware($container->get('config')['users']);
-});
-
-$container->set(Middleware\ErrorHandlerMiddleware::class, function (Container $container) {
-    return new Middleware\ErrorHandlerMiddleware($container->get('config')['debug']);
-});
+    Middleware\ErrorHandlerMiddleware::class => function (Container $container) {
+        return new Middleware\ErrorHandlerMiddleware($container->get('config')['debug']);
+    },
+];
