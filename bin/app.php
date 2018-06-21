e4ab63e382f95a166e7996ec85ec1929d8becc64
ElisDN <mail@elisdn.ru>
2018-06-21 13:22:04 +0300

    Imported Doctrine console commands

--- bin/app.php
+++ bin/app.php
@@ -1,6 +1,8 @@
 #!/usr/bin/env php
 <?php
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Symfony\Component\Console\Application;
 
 chdir(dirname(__DIR__));
@@ -12,6 +14,11 @@ require 'vendor/autoload.php';
 $container = require 'config/container.php';
 
 $cli = new Application('Application console');
+
+$entityManager = $container->get(EntityManagerInterface::class);
+$cli->getHelperSet()->set(new EntityManagerHelper($entityManager), 'em');
+
+Doctrine\ORM\Tools\Console\ConsoleRunner::addCommands($cli);
 
 $commands = $container->get('config')['console']['commands'];
 foreach ($commands as $command) {
